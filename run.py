9474ff4315a6192b12869fd346002d66ae78c6df
Alex Warren <amwarren@email.arizona.edu>
2015-04-26 00:12:31 -0700

    fixed bugs in finding the latest flight directory for pictures

--- run.py
+++ run.py
@@ -5,11 +5,11 @@ sys.path.insert(0, os.getcwd())
 from BuffelMapper.MapPhotographer import main
 
 log_dir = "./mavproxy_buffelmapper/logs"
-all_date_dirs = [d for d in os.listdir(log_dir) if os.path.isdir(d)]
+all_date_dirs = [os.path.join(log_dir,d) for d in os.listdir(log_dir) if os.path.isdir(os.path.join(log_dir, d))]
 latest_date_dir = max(all_date_dirs, key=os.path.getmtime)
-all_flight_dirs = [d for d in os.listdir(latest_date_dir) if os.path.isdir(d)]
-latest_flight_dir = max(all_date_dirs, key=os.path.getmtime)
-map_picture_dir = path.join(latest_flight_dir, "map_pics")
+all_flight_dirs = [os.path.join(latest_date_dir,d) for d in os.listdir(latest_date_dir) if os.path.isdir(os.path.join(latest_date_dir, d))]
+latest_flight_dir = max(all_flight_dirs, key=os.path.getmtime)
+map_picture_dir = os.path.join(latest_flight_dir, "map_pics")
 if not os.path.exists(map_picture_dir):
     os.makedirs(map_picture_dir)
 
